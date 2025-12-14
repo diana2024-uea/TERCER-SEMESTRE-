39cce06ab64a3e0150c1b12692e8d9472db2e09a
DIANA ESTEFANIA <de.ramonra@uea.educ.ec>
2025-12-14 15:31:58 -0500

    Comint

--- Semana3/Program.cs
+++ Semana3/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+
+// Clase Estudiante con sus atributos
+class Estudiante
+{
+    public int Id;
+    public string Nombres;
+    public string Apellidos;
+    public string Direccion;
+    public string[] Telefonos = new string[3]; // Array de 3 teléfonos
+
+    // Método para mostrar todos los datos del estudiante
+    public void MostrarDatos()
+    {
+        Console.WriteLine("ID: " + Id);
+        Console.WriteLine("Nombres: " + Nombres);
+        Console.WriteLine("Apellidos: " + Apellidos);
+        Console.WriteLine("Dirección: " + Direccion);
+        Console.WriteLine("Teléfonos:");
+        foreach (string telefono in Telefonos)
+        {
+            Console.WriteLine("- " + telefono);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Crear un objeto de tipo Estudiante
+        Estudiante estudiante = new Estudiante();
+
+        // Asignar valores a sus atributos
+        estudiante.Id = 1;
+        estudiante.Nombres = "Diana Estefania ";
+        estudiante.Apellidos = "Ramon Ramon";
+        estudiante.Direccion = "Av. Nueve de Octubre";
+
+        // Asignar valores al array de teléfonos
+        estudiante.Telefonos[0] = "0982932348";
+        estudiante.Telefonos[1] = "0987344321";
+        estudiante.Telefonos[2] = "0977722334";
+
+        // Llamar al método para mostrar los datos
+        estudiante.MostrarDatos();
+    }
+}
--- Semana3/obj/Debug/net10.0/Semana3.AssemblyInfo.cs
+++ Semana3/obj/Debug/net10.0/Semana3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+648989f47a9399cb055dc8f182c523df71ac16ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
